cfc977afc5d752acfb03f98221a486607628b029
NMadrid4 <nicolasmadrid123@outlook.com>
2018-11-02 10:01:01 -0500

    Diseñar vista compras

--- Beer/Controllers/Home/PurchasesViewController.swift
+++ Beer/Controllers/Home/PurchasesViewController.swift
@@ -8,22 +8,26 @@
 import UIKit
 
 class PurchasesViewController: UIViewController {
-
+    @IBOutlet weak var purchasesCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    }
+
+}
+extension PurchasesViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 8
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        cell.layer.cornerRadius = 8
+        cell.layer.borderWidth = 2.0
+        cell.layer.borderColor = UIColor.lightGray.cgColor
+        return cell
     }
-    */
-
+    
+    
 }
